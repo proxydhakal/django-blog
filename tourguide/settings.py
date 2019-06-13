d60cacc5eab595b213b210c60d537e1f472a8c0e
Shekhar Dhakal <33958757+proxydhakal@users.noreply.github.com>
2019-06-13 20:11:49 +0545

    Update settings.py

--- tourguide/settings.py
+++ tourguide/settings.py
@@ -81,17 +81,11 @@ WSGI_APPLICATION = 'tourguide.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tourguide',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
